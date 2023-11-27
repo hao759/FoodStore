ecd8636f33adf28fb8cf8c6da94c46c7a7ae4884
hao759 <qhao74154@gmail.com>
2023-11-27 23:12:50 +0700

    ViewComponent

---
+++ Views/Home/Components/MenuLoaiSPComponent/MenuLoaiSPComponent.cs
@@ -0,0 +1,22 @@
+﻿using CuaHangDoAn.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CuaHangDoAn.Views.Home.Components.MenuLoaiSPComponent
+{
+    public class MenuLoaiSPComponent: ViewComponent
+    {
+        private readonly CuaHangDoAnContext _context;
+
+        public MenuLoaiSPComponent(CuaHangDoAnContext context)
+        {
+            _context = context;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var listCategory = _context.CategoryProducts.AsNoTracking().ToList();
+            return View(listCategory);
+        }
+
+    }
+}
