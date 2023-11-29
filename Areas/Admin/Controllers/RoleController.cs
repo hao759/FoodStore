b97c7e9527d1625224d4490013e26dd743e5a449
hao759 <qhao74154@gmail.com>
2023-11-29 22:33:19 +0700

    Chỉnh 1 xíu

--- Areas/Admin/Controllers/RoleController.cs
+++ Areas/Admin/Controllers/RoleController.cs
@@ -37,7 +37,6 @@ namespace CuaHangDoAn.Areas.Admin.Controllers
         }
 
         // POST: Admin/Role/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
@@ -45,8 +44,6 @@ namespace CuaHangDoAn.Areas.Admin.Controllers
         {
             if (ModelState.IsValid)
             {
-                //_context.Add(appRole);
-                //await _context.SaveChangesAsync();
 
                 var newRole = new IdentityRole(appRole.Name);
                 await _roleManager.CreateAsync(newRole);
@@ -55,97 +52,43 @@ namespace CuaHangDoAn.Areas.Admin.Controllers
             return View(appRole);
         }
 
-        //// GET: Admin/Role/Edit/5
-        //public async Task<IActionResult> Edit(string id)
-        //{
-        //    if (id == null || _context.AppRole == null)
-        //    {
-        //        return NotFound();
-        //    }
 
-        //    var appRole = await _context.AppRole.FindAsync(id);
-        //    if (appRole == null)
-        //    {
-        //        return NotFound();
-        //    }
-        //    return View(appRole);
-        //}
 
-        //// POST: Admin/Role/Edit/5
-        //// To protect from overposting attacks, enable the specific properties you want to bind to.
-        //// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public async Task<IActionResult> Edit(string id, [Bind("Id,Name,NormalizedName,ConcurrencyStamp")] AppRole appRole)
-        //{
-        //    if (id != appRole.Id)
-        //    {
-        //        return NotFound();
-        //    }
+        // GET: Admin/Role/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null )
+            {
+                return NotFound();
+            }
 
-        //    if (ModelState.IsValid)
-        //    {
-        //        try
-        //        {
-        //            _context.Update(appRole);
-        //            await _context.SaveChangesAsync();
-        //        }
-        //        catch (DbUpdateConcurrencyException)
-        //        {
-        //            if (!AppRoleExists(appRole.Id))
-        //            {
-        //                return NotFound();
-        //            }
-        //            else
-        //            {
-        //                throw;
-        //            }
-        //        }
-        //        return RedirectToAction(nameof(Index));
-        //    }
-        //    return View(appRole);
-        //}
+            var appRole = await _roleManager.FindByIdAsync(id);
+               
+            if (appRole == null)
+            {
+                return NotFound();
+            }
 
-        //// GET: Admin/Role/Delete/5
-        //public async Task<IActionResult> Delete(string id)
-        //{
-        //    if (id == null || _context.AppRole == null)
-        //    {
-        //        return NotFound();
-        //    }
+            return View(appRole);
+        }
 
-        //    var appRole = await _context.AppRole
-        //        .FirstOrDefaultAsync(m => m.Id == id);
-        //    if (appRole == null)
-        //    {
-        //        return NotFound();
-        //    }
+        // POST: Admin/Role/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            //if (_roleManager.FindByIdAsync(id)== null)
+            //{
+            //    return Problem("Entity set 'CuaHangDoAnContext.AppRole'  is null.");
+            //}
+            var appRole = await _roleManager.FindByIdAsync(id); 
+            if (appRole != null)
+            {
+                await _roleManager.DeleteAsync(appRole);
+            }
 
-        //    return View(appRole);
-        //}
-
-        //// POST: Admin/Role/Delete/5
-        //[HttpPost, ActionName("Delete")]
-        //[ValidateAntiForgeryToken]
-        //public async Task<IActionResult> DeleteConfirmed(string id)
-        //{
-        //    if (_context.AppRole == null)
-        //    {
-        //        return Problem("Entity set 'CuaHangDoAnContext.AppRole'  is null.");
-        //    }
-        //    var appRole = await _context.AppRole.FindAsync(id);
-        //    if (appRole != null)
-        //    {
-        //        _context.AppRole.Remove(appRole);
-        //    }
-
-        //    await _context.SaveChangesAsync();
-        //    return RedirectToAction(nameof(Index));
-        //}
-
-        //private bool AppRoleExists(string id)
-        //{
-        //  return (_context.AppRole?.Any(e => e.Id == id)).GetValueOrDefault();
-        //}
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
